09d33557f438ef95e6ddf0e5992d8937e2cbea38
Tom Alessi <tom.alessi@gmail.com>
2015-08-01 12:12:36 -0700

    Fixing cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,7 @@
-name             'sd-route53'
+name             'opsworks-route53'
 maintainer       'Tom Alessi'
 maintainer_email 'tom.alessi@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures sd-route53 in an OpsWorks environment'
+description      'Installs/Configures opsworks-route53 in an OpsWorks environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
-depends "route53"
